816f909dbc95d6ad1b5f9ed74f1b5d1fe9956bf5
Gilbertly <gilbert@qubitping.com>
2021-01-19 12:03:37 +0300

    chore: wip open github pr with lambda fn

--- src/functions/github.ts
+++ src/functions/github.ts
@@ -1,7 +1,6 @@
 import { Context, CodePipelineEvent } from 'aws-lambda';
 import * as CodePipeline from 'aws-sdk/clients/codepipeline';
-import {Octokit} from '@octokit/core';
-import { request, RequestOptions } from 'https';
+import { Octokit } from '@octokit/core';
 
 let codePipelineClient: CodePipeline;
 
@@ -14,11 +13,11 @@ exports.handler = async (
 
   const repoName = process.env['GITHUB_REPO'] || '';
   const repoOwner = process.env['GITHUB_REPO_OWNER'] || '';
-  const gitSourceBranch = process.env['GITHUB_SOURCE_BRANCH'] || '';
-  const gitDestBranch = process.env['GITHUB_DEST_BRANCH'] || 'master';
+  const gitSourceBranch = process.env['GITHUB_SOURCE_BRANCH'] || 'develop';
+  const gitDestBranch = process.env['GITHUB_DEST_BRANCH'] || 'main';
   const authToken = process.env['GITHUB_OAUTH_TOKEN'] || '';
 
-  const octokit = new Octokit({auth: authToken});
+  const octokit = new Octokit({ auth: authToken });
   const pullRequestUrl = `POST /repos/${repoOwner}/${repoName}/pulls`;
   const jobID = event['CodePipeline.job'].id;
 
@@ -28,17 +27,20 @@ exports.handler = async (
       repo: repoName,
       head: gitSourceBranch,
       base: gitDestBranch,
-    })
-    console.log(`Created pr number: ${JSON.stringify(pullResponse.data.number)}`);
+    });
+    console.log(`PR number: ${JSON.stringify(pullResponse.data.number)}`);
 
-    return await codePipelineClient
+    const cpResponse = await codePipelineClient
       .putJobSuccessResult({
         jobId: jobID,
       })
       .promise();
+    console.log(`codepipeline success response: ${JSON.stringify(cpResponse)}`);
+
+    return cpResponse;
   } catch (error) {
     console.error(`Error creating pull request: ${error}`);
-    return await codePipelineClient
+    const cpResponse = await codePipelineClient
       .putJobFailureResult({
         jobId: jobID,
         failureDetails: {
@@ -48,87 +50,8 @@ exports.handler = async (
         },
       })
       .promise();
+    console.log(`codepipeline error response: ${JSON.stringify(cpResponse)}`);
+
+    return cpResponse;
   }
-};
-
-const createPullRequest = () => {
-  const repo = process.env['GITHUB_REPO'];
-  const repoOwner = process.env['GITHUB_REPO_OWNER'];
-
-  try {
-    const pullRequestBody = {
-      title: 'Automatic pull request by CI',
-      head: process.env['GITHUB_SOURCE_BRANCH'],
-      base: process.env['GITHUB_DEST_BRANCH'],
-    };
-    const path = `/repos/${repoOwner}/${repo}/pulls`;
-    githubRequest('POST', path, pullRequestBody);
-    return true;
-  } catch (err) {
-    if (err && Array.isArray(err.errors) && err.errors.length === 1) {
-      if (
-        err.errors[0].code === 'custom' &&
-        err.errors[0].message === 'No commits between master and staging'
-      ) {
-        // Nothing changed so no pull request.  This is fine.
-        return null;
-      }
-      if (
-        err.errors[0].code === 'custom' &&
-        /^A pull request already exists for /.test(err.errors[0].message)
-      ) {
-        // There's already a pull request for this.  This is fine.
-        return null;
-      }
-    }
-    throw err;
-  }
-};
-
-const mergePullRequest = (prNumber: string) => {
-  const repo = process.env['GITHUB_REPO'];
-  const repoOwner = process.env['GITHUB_REPO_OWNER'];
-  const path = `/repos/${repoOwner}/${repo}/pulls/${prNumber}/merge`;
-
-  const mergeBody = {
-    commit_message: 'Automatic PR merge by CI',
-  };
-  githubRequest('PUT', path, mergeBody);
-  return true;
-};
-
-const githubRequest = (
-  method: string,
-  path: string,
-  body?: Record<string, any>,
-) => {
-  const oauthToken = process.env['GITHUB_OAUTH_TOKEN'];
-  const bodyJson = JSON.stringify(body);
-  const options: RequestOptions = {
-    hostname: 'api.github.com',
-    port: 443,
-    path: path,
-    method: method,
-    headers: {
-      Authorization: `token ${oauthToken}`,
-      Accept: 'application/json',
-      'Content-Length': bodyJson.length,
-      'Content-Type': 'application/json',
-    },
-  };
-
-  const req = request(options, response => {
-    const responseBody: string[] = [];
-    console.log(`Request success! ${response.statusMessage}`);
-    response.setEncoding('utf8');
-    response.on('data', d => {
-      responseBody.push(d as string);
-    });
-  });
-
-  req.on('error', error => {
-    console.log(`Error with request! ${error.message}`);
-  });
-  req.write(bodyJson);
-  req.end();
 };
